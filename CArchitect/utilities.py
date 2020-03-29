b7f33233ea5a9cb8c1902b2f2147c3a64774019f
oscar-camacho <57529702+oscar-camacho@users.noreply.github.com>
2020-03-29 22:18:08 +0200

    Add files via upload

---
+++ CArchitect/utilities.py
@@ -0,0 +1,17 @@
+# In this module there are stored different types of data that are required to make conversions or assignation of values.
+
+
+# ID list
+ASCII_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ñÑçÇ'
+
+# Dictionaries
+protein_dict = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+           'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
+           'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
+           'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M', "UNK": "X"}
+
+
+dna_dict = {'DA': 'A', 'DC': 'C', 'DG': 'G', 'DT': 'T'}
+
+
+rna_dict = {'A': 'A', 'C': 'C', 'G': 'G', 'U': 'U'}
